1bdb3e78eff601397c481f2fffafeb5c178b647d
Megan McVey <megan.mcvey@shopify.com>
2021-08-18 15:19:47 -0400

    oops forgot to push this

--- check_shop.rb
+++ check_shop.rb
@@ -2,10 +2,14 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 
-doc = Nokogiri::HTML5(URI.open("https://canada.critrole.com/collections/all-products/products/baileys-dice-bag-of-hoarding?variant=36226702999718"))
+url = "https://canada.critrole.com/collections/all-products/products/baileys-dice-bag-of-hoarding?variant=36226702999718"
+doc = Nokogiri::HTML5(URI.open(url))
 script_tag = doc.at_css("#ProductJson-1")
 json = script_tag.inner_html
 object = JSON.parse(json)
 
-puts "#{object['available']}"
-
+if object['available']
+  puts "Product in Stock: #{url}"
+else
+  puts "Product out of Stock"
+end
