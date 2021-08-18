c3a058b3ddcca23b65106e7cabf3f88e3f9c46e7
Megan McVey <megan.mcvey@shopify.com>
2021-08-18 15:46:49 -0400

    Add option to check another url

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Check CritRole shop
 - `bundle install`
 - `ruby check_shop.rb`
 
-It will show Product in Stock with the url to get right to the store
+By default it will check the URL "https://canada.critrole.com/collections/all-products/products/baileys-dice-bag-of-hoarding?variant=36226702999718"
+
+If you pass a url on the command line it will check the availability of other products. Example
+
+- `ruby check_shop.rb https://canada.critrole.com/collections/all-products/products/vox-machina-dice-set-gm`
+
+It will either output "Product in Stock" with the url to get right to the store (if you use iTerm2, you can just Cmd-Click the url and it will open in your default browser)
 or
-Product out of Stock if it's unavailable
+"Product out of Stock" if it's unavailable
--- check_shop.rb
+++ check_shop.rb
@@ -1,15 +1,42 @@
 require 'open-uri'
 require 'nokogiri'
 require 'json'
+require 'optparse'
 
-url = "https://canada.critrole.com/collections/all-products/products/baileys-dice-bag-of-hoarding?variant=36226702999718"
-doc = Nokogiri::HTML5(URI.open(url))
-script_tag = doc.at_css("#ProductJson-1")
-json = script_tag.inner_html
-object = JSON.parse(json)
+class Application
+  Options = Struct.new(:url)
+  DEFAULT_URL = "https://canada.critrole.com/collections/all-products/products/baileys-dice-bag-of-hoarding?variant=36226702999718"
+  
+  def initialize
+    @url = DEFAULT_URL
+  end
+  
+  def run
+    parse_command_line
+    check_url
+  end
+  
+  def check_url
+    doc = Nokogiri::HTML5(URI.open(@url))
+    script_tag = doc.at_css("#ProductJson-1")
+    json = script_tag.inner_html
+    object = JSON.parse(json)
 
-if object['available']
-  puts "Product in Stock: #{url}"
-else
-  puts "Product out of Stock"
+    if object['available']
+      puts "Product in Stock: #{@url}"
+    else
+      puts "Product out of Stock"
+    end
+    
+  end
+  
+  private
+  
+  def parse_command_line
+    if ARGV.length > 0
+      @url = ARGV[0]
+    end
+  end
 end
+
+Application.new.run
